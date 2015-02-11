cfd3ff1a6781dd0cf4d84fae181f18dcf67616f1
Filip Petkovski <filip.petkovsky@gmail.com>
2015-02-11 18:51:18 +0100

    add local css/js files

--- platforms/android/ant-build/AndroidManifest.xml.d
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-C:\Users\filippe\Development\toptal-cordova\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Users\filippe\Development\toptal-cordova\platforms\android\AndroidManifest.xml \
+/home/philip/Development/Projects/toptal/platforms/android/ant-build/AndroidManifest.xml : \
+/home/philip/Development/Projects/toptal/platforms/android/AndroidManifest.xml \

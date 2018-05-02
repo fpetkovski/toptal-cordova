1a985bbe390c79bea6ee92bbef6f7d302e559859
Alexander Zinchuk <alx.zinchuk@gmail.com>
2018-05-02 14:34:05 +0300

    Remove platforms/ and plugins/ from source control

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/philip/Development/Projects/toptal/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/home/philip/Development/Projects/toptal/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/philip/Development/Projects/toptal/platforms/android/ant-build/AndroidManifest.xml : \
-/home/philip/Development/Projects/toptal/platforms/android/AndroidManifest.xml \
